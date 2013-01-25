5fa35387699fadced7f49ba7116e061719b83a37
David Warring <david.warring@gmail.com>
2013-01-25 17:24:21 +1100

    working on css1

--- lib/CSS/Grammar/CSS1.pm
+++ lib/CSS/Grammar/CSS1.pm
@@ -19,30 +19,40 @@ rule unary_operator {'-'|'+'}
 
 rule operator {'/'|','}
 
-rule property {<ident>}
-
 rule ruleset {
     <selector> [',' <selector>]*
-	'{' <declaration> [';' <declaration> ]* '}'
+	'{' <declaration> [';' <declaration> ]* ';'? '}'
 }
+
+rule property {<ident>}
 
 rule declaration {
      <property> ':' <expr> <prio>?
 }
 
-rule prio {\! important}
+rule expr { <term> [ <operator>? <term> ]* }
 
-regex selector {<simple_selector>+ <pseudo_element>?}
+rule term { <unary_operator>?
+		[ <length> | <string> | <percentage> 
+		  | <num> | $<ems>='em' | $<exs>='ex' | <ident> | <hexcolor> | <url> | <rgb> ] }
 
-regex simple_selector {[<element_name> <id>? <class>? <pseudo_class>?]
-	| [ <id> <class>? <pseudo_class>?]
-	| [ <class> <pseudo_class>?]
-	| <pseudo_class>}
+rule hexcolor {<id>}
+
+rule rgb{ 'rgb' '(' <num>('%'?) ','  <num>('%'?) ','  <num>('%'?) ')' }
+
+rule prio {\!important}
+
+regex selector {<simple_selector>[<ws><simple_selector>]* <pseudo_element>?}
+
+regex simple_selector { <element_name> <id>? <class>? <pseudo_class>?
+	| <id> <class>? <pseudo_class>?
+	| <class> <pseudo_class>?
+	| <pseudo_class> }
 
 rule element_name {<ident>}
 
 rule  pseudo_class      {':'(link|visited|active)}
-rule  pseudo_element    {<ident>?':'(first\-[line|letter])}
+rule  pseudo_element    {':'(first\-[line|letter])}
 
 rule url { 'url(' [<string>|<unquoted_string>] ')' }
 
@@ -50,23 +60,23 @@ rule url { 'url(' [<string>|<unquoted_string>] ')' }
 
 rule unicode		{'\\'<[0..9 a..f A..F]>**1..4}
 rule latin1		{<[\o241..\o377]>}
-rule  escape		{<unicode>|'\\'<[' '..~¡..ÿ]>}
-rule  stringchar	{<escape>|<latin1>|<[\o40 \! \# \$ \% \& \( .. \~]>}
-rule  nmstrt		{<[a..z A..Z]>|<latin1>|<escape>}
-rule  nmchar		{<[\- a..z A..Z 0..9]>|<latin1>|<escape>}
-rule  ident		{<nmstrt><nmchar>*}
-rule  name		{<nmchar>+}
+rule escape		{<unicode>|'\\'<[' '..~¡..ÿ]>}
+rule stringchar	{<escape>|<latin1>|<[\o40 \! \# \$ \% \& \( .. \~]>}
+rule nmstrt		{<[a..z A..Z]>|<latin1>|<escape>}
+rule nmchar		{<[\- a..z A..Z 0..9]>|<latin1>|<escape>}
+rule ident		{<nmstrt><nmchar>*}
+rule name		{<nmchar>+}
 rule d			{<[0..9]>}
-rule  notnm		{<-[\- a..z A..Z 0..9\\]>|<latin1>}
-rule num		{<d>+|[<d>*\.<d>+]}
-rule  string		{\"(<stringchar>|\')*\" | \'(<stringchar>|\")*\'}
+rule notnm		{<-[\- a..z A..Z 0..9\\]>|<latin1>}
+rule num		{[<d>*\.]?<d>+}
+rule string		{\"(<stringchar>|\')*\" | \'(<stringchar>|\")*\'}
 
-rule  quotable_char  {<ws_char> | <[\, \' \" \( \) \\ ]>}
-rule  unquoted_escape_seq   {'\\'<quotable_char>?}
-rule  unquoted_string       {[<-quotable_char>|<unquoted_escape_seq>]+}
+rule quotable_char  {<ws_char> | <[\, \' \" \( \) \\ ]>}
+rule unquoted_escape_seq   {'\\'<quotable_char>?}
+rule unquoted_string       {[<-quotable_char>|<unquoted_escape_seq>]+}
 
-rule  percentage        {<num>'%'}
-rule  length            {<num>(pt|mm|cm|pc|in|px|em|ex)}
+rule percentage        {<num>'%'}
+rule length            {<num>(pt|mm|cm|pc|in|px|em|ex)}
 
 rule id                 {'#'<name>}
 rule class              {'.'<name>}
