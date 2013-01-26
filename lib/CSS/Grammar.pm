cabae022a5f719c465b5d5f6146ef14f5279fe50
David Warring <david.warring@gmail.com>
2013-01-26 18:22:38 +1100

    Moved 'lexer' into base grammar

--- lib/CSS/Grammar.pm
+++ lib/CSS/Grammar.pm
@@ -15,4 +15,31 @@ grammar CSS::Grammar {
 	<ws_char>*}
 
 
+    # "lexer"
+
+    rule unicode	{'\\'<[0..9 a..f A..F]>**1..4}
+    rule latin1		{<[\o241..\o377]>}
+    rule escape		{<unicode>|'\\'<[' '..~¡..ÿ]>}
+    rule stringchar	{<escape>|<latin1>|<[\o40 \! \# \$ \% \& \( .. \~]>}
+    rule nmstrt		{<[a..z A..Z]>|<latin1>|<escape>}
+    rule nmchar		{<[\- a..z A..Z 0..9]>|<latin1>|<escape>}
+    rule ident		{<nmstrt><nmchar>*}
+    rule name		{<nmchar>+}
+    rule d		{<[0..9]>}
+    rule notnm		{<-[\- a..z A..Z 0..9\\]>|<latin1>}
+    rule num		{[<d>*\.]?<d>+}
+    rule string		{\"(<stringchar>|\')*\" | \'(<stringchar>|\")*\'}
+
+    rule id             {'#'<name>}
+    rule class          {'.'<name>}
+
+    rule percentage     {<num>'%'}
+    rule length         {<num>(pt|mm|cm|pc|in|px|em|ex)}
+
+    # unquoted strings - as permitted in urls
+    rule quotable_char         {<ws_char> | <[\, \' \" \( \) \\ ]>}
+    rule unquoted_escape_seq   {'\\'<quotable_char>?}
+    rule unquoted_string       {[<-quotable_char>|<unquoted_escape_seq>]+}
+    rule text {<string>|<unquoted_string>}
+
 }
--- lib/CSS/Grammar/CSS1.pm
+++ lib/CSS/Grammar/CSS1.pm
@@ -7,78 +7,53 @@ grammar CSS::Grammar::CSS1 is CSS::Grammar {
 # as defined in w3c Appendix B: CSS1 Grammar
 # http://www.w3.org/TR/REC-CSS1/#appendix-b
 
-rule TOP {^ <stylesheet> $}
+    rule TOP {^ <stylesheet> $}
 
-# productions
+    # productions
 
-rule stylesheet {<import>* <ruleset>*}
+    rule stylesheet {<import>* <ruleset>*}
+ 
+    rule import { \@import [<string>|<url>] ';' }
 
-rule import { \@import [<string>|<url>] ';' }
+    rule unary_operator {'-'|'+'}
 
-rule unary_operator {'-'|'+'}
+    rule operator {'/'|','}
 
-rule operator {'/'|','}
+    rule ruleset {
+	<selector> [',' <selector>]*
+	    '{' <declaration> [';' <declaration> ]* ';'? '}'
+    }
 
-rule ruleset {
-    <selector> [',' <selector>]*
-	'{' <declaration> [';' <declaration> ]* ';'? '}'
-}
+    rule property {<ident>}
 
-rule property {<ident>}
+    rule declaration {
+	 <property> ':' <expr> <prio>?
+    }
 
-rule declaration {
-     <property> ':' <expr> <prio>?
-}
+    rule expr { <term> [ <operator>? <term> ]* }
 
-rule expr { <term> [ <operator>? <term> ]* }
+    rule term { <unary_operator>?
+		    [ <length> | <string> | <percentage>
+		      | <num> | $<ems>='em' | $<exs>='ex' | <ident> | <hexcolor> | <url> | <rgb> ]}
 
-rule term { <unary_operator>?
-		[ <length> | <string> | <percentage> 
-		  | <num> | $<ems>='em' | $<exs>='ex' | <ident> | <hexcolor> | <url> | <rgb> ] }
+    rule hexcolor {<id>}
 
-rule hexcolor {<id>}
+    rule rgb{ 'rgb' '(' <num>('%'?) ','  <num>('%'?) ','  <num>('%'?) ')' }
 
-rule rgb{ 'rgb' '(' <num>('%'?) ','  <num>('%'?) ','  <num>('%'?) ')' }
+    rule prio {\!important}
 
-rule prio {\!important}
+    regex selector {<simple_selector>[<ws><simple_selector>]* <pseudo_element>?}
 
-regex selector {<simple_selector>[<ws><simple_selector>]* <pseudo_element>?}
+    regex simple_selector { <element_name> <id>? <class>? <pseudo_class>?
+	    | <id> <class>? <pseudo_class>?
+	    | <class> <pseudo_class>?
+	    | <pseudo_class> }
 
-regex simple_selector { <element_name> <id>? <class>? <pseudo_class>?
-	| <id> <class>? <pseudo_class>?
-	| <class> <pseudo_class>?
-	| <pseudo_class> }
+    rule element_name {<ident>}
 
-rule element_name {<ident>}
+    rule  pseudo_class      {':'(link|visited|active)}
+    rule  pseudo_element    {':'(first\-[line|letter])}
 
-rule  pseudo_class      {':'(link|visited|active)}
-rule  pseudo_element    {':'(first\-[line|letter])}
-
-rule url { 'url(' [<string>|<unquoted_string>] ')' }
-
-# "lexer"
-
-rule unicode		{'\\'<[0..9 a..f A..F]>**1..4}
-rule latin1		{<[\o241..\o377]>}
-rule escape		{<unicode>|'\\'<[' '..~¡..ÿ]>}
-rule stringchar	{<escape>|<latin1>|<[\o40 \! \# \$ \% \& \( .. \~]>}
-rule nmstrt		{<[a..z A..Z]>|<latin1>|<escape>}
-rule nmchar		{<[\- a..z A..Z 0..9]>|<latin1>|<escape>}
-rule ident		{<nmstrt><nmchar>*}
-rule name		{<nmchar>+}
-rule d			{<[0..9]>}
-rule notnm		{<-[\- a..z A..Z 0..9\\]>|<latin1>}
-rule num		{[<d>*\.]?<d>+}
-rule string		{\"(<stringchar>|\')*\" | \'(<stringchar>|\")*\'}
-
-rule quotable_char  {<ws_char> | <[\, \' \" \( \) \\ ]>}
-rule unquoted_escape_seq   {'\\'<quotable_char>?}
-rule unquoted_string       {[<-quotable_char>|<unquoted_escape_seq>]+}
-
-rule percentage        {<num>'%'}
-rule length            {<num>(pt|mm|cm|pc|in|px|em|ex)}
-
-rule id                 {'#'<name>}
-rule class              {'.'<name>}
+    rule url  { 'url(' <text> ')' }
 
 }
