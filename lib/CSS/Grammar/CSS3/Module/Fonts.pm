3381abfa60db9238ff3b366d5166832f8c5e38b0
David Warring <david.warring@gmail.com>
2013-03-03 16:04:04 +1100

    tidied header; added version

--- lib/CSS/Grammar/CSS3/Module/Fonts.pm
+++ lib/CSS/Grammar/CSS3/Module/Fonts.pm
@@ -1,12 +1,12 @@
 use v6;
 
-# CSS3 Font Module Extensions
-# - see http://www.w3.org/TR/css3-fonts/
+# CSS3 Font Extension Module
+# specification: http://www.w3.org/TR/2013/WD-css3-fonts-20130212/
 #
 # nb this standard is under revision (as of Feb 2013). Biggest change
 # is the proposed at-rule @font-feature-values
 
-grammar CSS::Grammar::CSS3::Module::Fonts {
+grammar CSS::Grammar::CSS3::Module::Fonts:ver<20130212.000> {
     rule at_rule:sym<font_face> { \@(:i'font-face') <declarations> }
 }
 
