86dceaa399e3f056518539ec52df7709a7ebd706
David Warring <david.warring@gmail.com>
2013-08-22 07:23:10 +1000

    revamped Test harness - delegated parsing and added try/CATCH

--- lib/CSS/Grammar/Test.pm
+++ lib/CSS/Grammar/Test.pm
@@ -5,74 +5,95 @@ module CSS::Grammar::Test {
     use Test;
     use JSON::Tiny;
 
-    our sub parse_tests($input, $parse,
-                         :$rule, :$suite, :%expected, :@warnings) {
+    our sub parse-tests($class, $input, :$parse, :$actions,
+			:$rule, :$suite, :%expected) {
 
-        my $expected_parse = %expected<parse> // $input;
+	my $p = $parse;
 
-        if (defined $input) {
-            my $input_display = $input.chars > 300 ?? $input.substr(0,50) ~ "     ......    "  ~ $input.substr(*-50) !! $input;
-            my $got = $parse.trim;
-            my $expected = $expected_parse.trim;
-            is($got, $expected, "{$suite}: " ~ $rule ~ " parse: " ~ $input_display)
-        }
-        else {
-            ok($parse.Str, "{$suite}: " ~ $rule ~ " parsed")
-        }
+	try {
+	    $actions.reset if $actions.can('reset');
 
-        if  %expected.exists('warnings') && ! %expected<warnings>.defined {
-            diag "untested warnings: " ~ @warnings
-                if @warnings;
-        }
-        else {
-            todo( %expected<warnings-todo> )
-                if %expected<warnings-todo>;
-     
-            if %expected<warnings>.isa('Regex') {
-                my @matched = ([~] @warnings).match(%expected<warnings>);
-                ok( @matched, "{$suite} warnings")
-                    or diag @warnings;
-            }
-            else {
-                my @expected_warnings = %expected<warnings> // ();
-                is(@warnings, @expected_warnings,
-                   @expected_warnings ?? "{$suite} warnings" !! "{$suite} no warnings");
-            }
-	}
+	    $p //= $class.parse( $input, :rule($rule), :actions($actions));
 
-        if defined (my $ast = %expected<ast>) {
-            if my $todo-ast = %expected<todo><ast> {
-                todo($todo-ast);
-            }
-	    if %*ENV<CSS_XT> {
-		# test json canonicalization - thorough, but slower
-		is( to-json($parse.ast), to-json($ast), "{$suite} - ast");
+	    my @warnings = $actions.warnings
+		if $actions.can('warnings');
+
+	    my $expected-parse = %expected<parse> // $input;
+
+	    if (defined $input) {
+		my $input-display = $input.chars > 300 ?? $input.substr(0,50) ~ "     ......    "  ~ $input.substr(*-50) !! $input;
+		my $got = $p.Str.trim;
+		my $expected = $expected-parse.trim;
+		is($got, $expected, "{$suite}: " ~ $rule ~ " parse: " ~ $input-display)
 	    }
 	    else {
-		# just test stringification
-		is( $parse.ast.Str, $ast.Str, "{$suite} - ast")
-		    or diag to-json($parse.ast);
+		ok($p.Str, "{$suite}: " ~ $rule ~ " parsed")
 	    }
-        }
-        else {
-            if defined $parse.ast {
-                note 'untested_ast: ' ~ to-json( $parse.ast )
-                    unless %expected.exists('ast');
-            }
-            else {
-                diag "no {$suite} ast: " ~ ($input // '');
-            }
-        }
 
-        if defined (my $token = %expected<token>) {
-            if ok($parse.ast.can('units'), "{$suite} is a token") {
-                if my $units = %$token<units> {
-                    is($parse.ast.units, $units, "{$suite} - units: " ~$units);
-                }
-                if my $type = %$token<type> {
-                    is($parse.ast.type, $type, "{$suite} - type: " ~$type);
-                }
-            }
-        }
+	    if  %expected.exists('warnings') && ! %expected<warnings>.defined {
+		diag "untested warnings: " ~ @warnings
+		    if @warnings;
+	    }
+	    else {
+		todo( %expected<warnings-todo> )
+		    if %expected<warnings-todo>;
+     
+		if %expected<warnings>.isa('Regex') {
+		    my @matched = ([~] @warnings).match(%expected<warnings>);
+		    ok( @matched, "{$suite} warnings")
+			or diag @warnings;
+		}
+		else {
+		    my @expected_warnings = %expected<warnings> // ();
+		    is(@warnings, @expected_warnings,
+		       @expected_warnings ?? "{$suite} warnings" !! "{$suite} no warnings");
+		}
+	    }
+
+	    if defined (my $ast = %expected<ast>) {
+		if my $todo-ast = %expected<todo><ast> {
+		    todo($todo-ast);
+		}
+		if %*ENV<CSS_XT> {
+		    # test json canonicalization - thorough, but slower
+		    is( to-json($p.ast), to-json($ast), "{$suite} - ast");
+		}
+		else {
+		    # just test stringification
+		    is( $p.ast.Str, $ast.Str, "{$suite} - ast")
+			or diag to-json($p.ast);
+		}
+	    }
+	    else {
+		if defined $p.ast {
+		    note 'untested_ast: ' ~ to-json( $p.ast )
+			unless %expected.exists('ast');
+		}
+		else {
+		    diag "no {$suite} ast: " ~ ($input // '');
+		}
+	    }
+
+	    if defined (my $token = %expected<token>) {
+		if ok($p.ast.can('units'), "{$suite} is a token") {
+		    if my $units = %$token<units> {
+			is($p.ast.units, $units, "{$suite} - units: " ~$units);
+		    }
+		    if my $type = %$token<type> {
+			is($p.ast.type, $type, "{$suite} - type: " ~$type);
+		    }
+		}
+	    }
+
+	    CATCH {
+		default {
+		    note "parse failure: $_";
+		    ok(False, "{$suite}: " ~ $rule ~ " parsed");
+		}
+	    }
+	}	
+
+	return $parse;
     }
+
 }
