c18b5923b3357fbb757ea634e97481269076a077
David Warring david.warring@gmail.com <david.warring@gmail.com>
2014-03-04 10:08:02 -0800

    robuster parse failure detection - fixes recent rakudo failures

--- lib/CSS/Grammar/Test.pm
+++ lib/CSS/Grammar/Test.pm
@@ -20,66 +20,67 @@ module CSS::Grammar::Test {
 	    my @warnings = $actions.warnings
 		if $actions.can('warnings');
 
-	    my $expected-parse = %expected<parse> // $input;
+	    my $parsed = $p.defined && ~$p ne '';
 
-	    if defined $input {
-		my $input-display = $input.chars > 300 ?? $input.substr(0,50) ~ "     ......    "  ~ $input.substr(*-50) !! $input;
-		my $got = (~$p).trim;
-		my $expected = $expected-parse.trim;
-		is($got, $expected, "{$suite}: " ~ $rule ~ " parse: " ~ $input-display)
-	    }
-	    else {
-		ok(~$p, "{$suite}: " ~ $rule ~ " parsed")
-	    }
+	    ok(~$parsed, "{$suite}: " ~ $rule ~ " parsed");
 
-	    if  %expected<warnings>:exists && ! %expected<warnings>.defined {
-		diag "untested warnings: " ~ @warnings
-		    if @warnings;
-	    }
-	    else {
-		todo( %expected<warnings-todo> )
-		    if %expected<warnings-todo>;
+	    if $parsed {
+		if defined $input {
+		    my $input-display = $input.chars > 300 ?? $input.substr(0,50) ~ "     ......    "  ~ $input.substr(*-50) !! $input;
+		    my $got = (~$p).trim;
+		    my $expected-parse = (%expected<parse> // $input).trim;
+		    is($got, $expected-parse, "{$suite}: " ~ $rule ~ " parse: " ~ $input-display)
+		}
+
+		if  %expected<warnings>:exists && ! %expected<warnings>.defined {
+		    diag "untested warnings: " ~ @warnings
+			if @warnings;
+		}
+		else {
+		    todo( %expected<warnings-todo> )
+			if %expected<warnings-todo>;
      
-		if %expected<warnings>.isa('Regex') {
-		    my @matched = ([~] @warnings).match(%expected<warnings>);
-		    ok( @matched, "{$suite} warnings")
-			or diag @warnings;
-		}
-		else {
-		    my @expected_warnings = %expected<warnings> // ();
-		    is(@warnings, @expected_warnings,
-		       @expected_warnings ?? "{$suite} warnings" !! "{$suite} no warnings");
-		}
-	    }
-
-	    if defined (my $ast = %expected<ast>) {
-		if my $todo-ast = %expected<todo><ast> {
-		    todo($todo-ast);
-		}
-		if %*ENV<CSS_XT> {
-		    # test json canonicalization - thorough, but slower
-		    is( to-json($p.ast), to-json($ast), "{$suite} - ast");
-		}
-		else {
-		    # just test stringification
-		    is( ~$p.ast, ~$ast, "{$suite} - ast")
-			or diag to-json($p.ast);
-		}
-	    }
-	    else {
-		if defined $p.ast {
-		    note 'untested_ast: ' ~ to-json( $p.ast )
-			unless %expected<ast>:exists;
-		}
-	    }
-
-	    if defined (my $token = %expected<token>) {
-		if ok($p.ast.can('units'), "{$suite} is a token") {
-		    if my $units = %$token<units> {
-			is($p.ast.units, $units, "{$suite} - units: " ~$units);
+		    if %expected<warnings>.isa('Regex') {
+			my @matched = ([~] @warnings).match(%expected<warnings>);
+			ok( @matched, "{$suite} warnings")
+			    or diag @warnings;
 		    }
-		    if my $type = %$token<type> {
-			is($p.ast.type, $type, "{$suite} - type: " ~$type);
+		    else {
+			my @expected_warnings = %expected<warnings> // ();
+			is(@warnings, @expected_warnings,
+			   @expected_warnings ?? "{$suite} warnings" !! "{$suite} no warnings");
+		    }
+		}
+
+		if defined (my $ast = %expected<ast>) {
+		    if my $todo-ast = %expected<todo><ast> {
+			todo($todo-ast);
+		    }
+		    if %*ENV<CSS_XT> {
+			# test json canonicalization - thorough, but slower
+			is( to-json($p.ast), to-json($ast), "{$suite} - ast");
+		    }
+		    else {
+			# just test stringification
+			is( ~$p.ast, ~$ast, "{$suite} - ast")
+			    or diag to-json($p.ast);
+		    }
+		}
+		else {
+		    if defined $p.ast {
+			note 'untested_ast: ' ~ to-json( $p.ast )
+			    unless %expected<ast>:exists;
+		    }
+		}
+
+		if defined (my $token = %expected<token>) {
+		    if ok($p.ast.can('units'), "{$suite} is a token") {
+			if my $units = %$token<units> {
+			    is($p.ast.units, $units, "{$suite} - units: " ~$units);
+			}
+			if my $type = %$token<type> {
+			    is($p.ast.type, $type, "{$suite} - type: " ~$type);
+			}
 		    }
 		}
 	    }
